ab23ed701ef5c8bcdc81382874258cb7271a02de
it21001802 <89139730+it21001802@users.noreply.github.com>
2021-10-09 22:49:41 +0530

    home update

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -3,21 +3,21 @@ function changeImage(){
 	var Htext=document.getElementById("body_text")
 	if (Img.src.match("Images/bodyImage1.jpg")){
 		Img.src="Images/bodyImage2.jpg";
-		Htext.innerHTML="Join with us to improve your business<br>We have a qualified team to<br> create your content as <br>YOU<br> wish ";
+		Htext.innerHTML="<br>We do understand your desires <br>and <br>we are bound to fulfil your needs <br>with our products";
 	
 	}
 	else if(Img.src.match("Images/bodyImage2.jpg")){
 		Img.src="Images/bodyImage3.jpg";
-		Htext.innerHTML="We have a qualified team to<br> create your content as <br>YOU<br> wish ";
+		Htext.innerHTML="Ad Spaces is an award wininng <br>guarented company that combines <br>creativity,technology,and <br>trustworthy service <br>under one roof";
 	
 	}
 	else if ((Img.src.match("Images/bodyImage3.jpg"))){
 		Img.src="Images/bodyImage4.jpg";
-		Htext.innerHTML="Did you know <br>how Important is VISUAL CONTENT<br>We have created content that <br>has insprie many peoples <br>We provide profecational Services on <br><br>Creating Advertisment<br>Creating Video<br>Creating Banner</p>"
+		Htext.innerHTML="<br>Join with us to improve your business<br>We have a qualified team to<br> create your content  <br>AS YOU WISH </p>"
 	}
 	else {
 		Img.src="Images/bodyImage1.jpg";
-		Htext.innerHTML="Do you know <br>how Important is VISUAL CONTENT<br>We have created content that <br>has insprie many peoples <br>We provide profecational Services on <br><br>Creating Advertisment<br>Creating Video<br>Creating Banner</p>"
+		Htext.innerHTML="We have created content that <br>has insprie many peoples <br>We provide profecational Services on <br>Creating Advertisment<br>Creating Video<br>Creating Banner</p>"
 		
 	}
 }
