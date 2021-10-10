488d625d16ea66bc5c740a73783b0577cd21d4c9
it21097010 <87365705+it21097010@users.noreply.github.com>
2021-10-10 19:35:29 +0530

    Update database connection

--- AdditionalDetails.php
+++ AdditionalDetails.php
@@ -2,28 +2,33 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$dbname = "testad";
+$dbname = "adspace";
 
 $conn = mysqli_connect($server,$username,$password,$dbname);
 
+if($conn->connect_error){
+    die("Connection failed: " . $con->connect_error);
+}
 if(isset($_POST['submit'])){
-    $massage=$_POST['massage'];
-    $phone_number=$_POST['phone_number'];
-    $email=$_POST['email'];
 
-    $query = "INSERT INTO details VALUES($massage,$phone_number,$email)";
+   
+    $phone_number=$_POST['PhoneNumber'];
+   $date=$_POST['date'];
+   $massage=$_POST['InstructionsToEditor'];
+   $filename = $_FILES["files_upload"]["name"];
+    $tempname = $_FILES["files_upload"]["tmp_name"];    
+    $folder = "C:/xampp/htdocs/Adspace/".$filename;
 
-    $run = mysqli_query($conn,$query) or die(mysqli_error());
-
-    if($run){
-        echo "form submitted succesfully";
+    $sql = "INSERT INTO addtionaldetails (PhoneNumber,date,massage,File_Name)VALUES('$phone_number','$date','$massage','$filename')";
+  
+  
+    $conn->query($sql);
+    if (move_uploaded_file($tempname, $folder))  {
+        header('location:Cart.html');
+    }else{
+       echo "Failed to upload file";
     }
-    else{
-        echo "form not submitted";
-    }
-    
-    }
-
+}
 
 
 ?>
