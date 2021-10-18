4be282689b8b3d236e1feab30aa76770a9046fb5
it21097010 <87365705+it21097010@users.noreply.github.com>
2021-10-19 01:16:08 +0530

    fixed session problems

---
+++ dashboard.php
@@ -0,0 +1,79 @@
+<?php
+require 'index.php';
+ session_start();
+ 
+ 
+ if(isset($_SESSION['id'])){
+   
+     
+
+ }
+   else{
+   
+       echo"<script>location.href='signin.html'</script>";
+   }
+?>
+<?php
+$id=$_SESSION['id'];
+$data="SELECT Username,First_Name FROM member WHERE Member_ID='$id'";
+$result=$conn->query($data) or die(mysqli_error());
+if ($result-> num_rows>0){
+	while($row=$result->fetch_assoc()){
+		
+		$username=$row['Username'];
+		$name=$row['First_Name'];
+		
+	}
+	
+}
+?>
+<!DOCTYPE html>
+
+<head>
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="Styles/dashboard.css" />
+  <link rel="stylesheet" href="Styles/Home.css" />
+  
+
+</head>
+
+<body>
+  <a href="Home.html"><img class="logo" src="Images/logo.jpeg" alt="logo" width:"10%" height="150px"></a>
+  <button class="button1" ><a href="logout.php" style="text-decoration:none">Log out</a></button>
+  <button class="button3"><a href="contactus.html" style="text-decoration:none">Contact Us</a></button>
+  <div>
+    <input type="text" class="search" placeholder="Search item">
+    <button class="search_1"><img src="Images/search_Icon.png" height="25px" width="25px"></button>
+  </div>
+  <font color="white" class="title1" size="6px" face="arial">
+    <center><b>Online Advertising <br>Agency<b></center>
+  </font>
+
+ 
+
+  <ul class="menu">
+    <li class="menu"><a href="Cart.html">My cart</li></a>
+    <li class="menu"><a href="About us.html">About Us</li></a>
+    <li class="menu"><a href="category.html">Catergories</li></a>
+    <li class="menu"><a href="projects.html">Projects</li></a>
+    <li class="menu"><a href="Home.html">Home</li></a>
+  </ul>
+<font color="white">
+  <center><hr><h1>Welcome <?php echo"$username"?></h1></center>
+  
+</font>
+<br>  
+  <hr>
+  
+  <div class="footer" id="foo" >
+    <img src="Images/logo.jpeg" width="8%">
+    <font align="left" a href="mailto:adspace@contactus.com"><b>Email:adspace@contactus.com</b><br>
+      &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Phone Number: +94 234-567-8944</font>
+    <font class="footerText"><a href="aboutus.html">About Us</a></font>
+    <font class="footerText1"><a href="contactus.html">Contact Us</a></font>
+  </div>
+
+</body>
+
+
+</html>
--- signin.php
+++ signin.php
@@ -3,20 +3,22 @@ require 'index.php';
 session_start();
 
 if(isset($_SESSION['Username'])){
-    header ('Location:dashboard.html');
+    header ('Location:dashboard.php');
     }
 else{
     $username=$_POST['Username'];
     $password=$_POST['Password'];
-    $sql="SELECT Member_ID,First_Name FROM member WHERE Username='$username' AND Password='$password'";
+    $sql="SELECT Member_ID,Username,First_Name FROM member WHERE Username='$username' AND Password='$password'";
 		$result = mysqli_query($conn,$sql) or die(mysqli_error());
         if(mysqli_num_rows($result) > 0){
             while($row = mysqli_fetch_assoc($result)) {
-                $_SESSION['id'] = $row["Username"];
-                $_SESSION['username'] = $row["First_Name"];
+                $_SESSION['id']=$row["Member_ID"];
+                $_SESSION['Username'] = $row["Username"];
+                $_SESSION['First_Name'] = $row["First_Name"];
     
             }
-        header('location:packages.html');}
+            echo $_SESSION['id'];
+        header('location:dashboard.php');}
         else{
             echo "<script> alert('Username or Password incorrect!') </script>";	
             echo"<script>location.href='Home.html'</script>";
