d374df7091e816b2a3494eda755716c7ae920544
it21097010 <87365705+it21097010@users.noreply.github.com>
2021-10-19 09:08:20 +0530

    updated

--- customizeVideo.php
+++ customizeVideo.php
@@ -1,11 +1,25 @@
 <?php
 require 'index.php';
+ session_start();
+ 
+ 
+ if(isset($_SESSION['id'])){
+   
+     
 
+ }
+   else{
+   
+       echo"<script>location.href='signin.html'</script>";
+   }
+?>
+<?php
+require 'index.php';
+$id=$_SESSION['id'];
 
 if(isset($_POST['2Kvideo'])){
     
-
-$sql="INSERT INTO cart (Item_id,Item_Name,Price,Item_Description) VALUES('1','2K Video',30000,'5 to 10 minutes video in 2K resolution')";
+$sql="INSERT INTO cart (Item_id,Item_Name,Price,Item_Description,Member_ID) VALUES('1','2K Video',30000,'5 to 10 minutes video in 2K resolution','$id')";
 $conn->query($sql);
 
 if($conn->query($sql))
@@ -22,7 +36,7 @@ if($conn->query($sql))
 if(isset($_POST['4Kvideo'])){
     
 
-    $sql="INSERT INTO cart (Item_id,Item_Name,Price,Item_Description) VALUES('2','4K Video',50000,'5 to 12 minutes video in 4K resolution')";
+    $sql="INSERT INTO cart (Item_id,Item_Name,Price,Item_Description,Member_ID) VALUES('2','4K Video',50000,'5 to 12 minutes video in 4K resolution','$id')";
     $conn->query($sql);
     
     if($conn->query($sql))
@@ -37,7 +51,7 @@ if(isset($_POST['4Kvideo'])){
     if(isset($_POST['8Kvideo'])){
     
 
-        $sql="INSERT INTO cart (Item_id,Item_Name,Price,Item_Description) VALUES('3','8K Video',150000,'10 to 20 minutes video in 8K resolution')";
+        $sql="INSERT INTO cart (Item_id,Item_Name,Price,Item_Description,Member_ID) VALUES('3','8K Video',150000,'10 to 20 minutes video in 8K resolution','$id')";
         $conn->query($sql);
         
         if($conn->query($sql))
